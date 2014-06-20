ce774ebbfeb7aca4eefee12bb77ef36a0f8e3525
Kishorekumar Neelamegam <nkishore@megam.co.in>
2014-06-20 11:41:24 +0530

    debian bundle for megamsnowflake, and new package megamcommon

--- package/pkg.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-rm *.deb
-fpm -s dir -t deb -n "megamsnowflake" -v 0.5 -d "python,python-thrift" --after-install ./postinst --deb-upstart ./etc/init/snowflake --license "Apache V2" --vendor "Megam Systems" --maintainer "<rajthilak@megam.co.in>" --url "http://www.gomegam.com" --description "Python based unique id generator server based on Twitters snowflake. source : https://github.com/erans/pysnowflake.git" ./usr/share/megam/snowflake/bin
